60a4bc4951631a82e796cd335ef537ba9c51cc16
Emanuel Correia <guardeded@gmail.com>
2025-06-05 11:44:06 +0100

    added a details card for the project

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+// Navigation bar animations and controllers
+
 const navAnimation = [
     ".sidebar",
     ".hamburger",
@@ -22,3 +24,20 @@ $(".nav-overlay").on("click", () => {
         }
     }
 });
+
+// Projects section
+
+const netmattersButton = document.querySelector("#netmatters-details");
+const netmattersCard = document.querySelector("#netmatters-card");
+const projectCards = document.querySelector(".projects-work");
+const cardCloseButton = document.querySelectorAll(".details-header .button");
+
+cardCloseButton[0].addEventListener("click", () => {
+    netmattersCard.style.display = "none"
+    projectCards.style.display = ""
+})
+
+netmattersButton.addEventListener("click", () => {
+    netmattersCard.style.display = ""
+    projectCards.style.display = "none"
+})
