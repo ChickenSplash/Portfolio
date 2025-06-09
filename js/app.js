6c2a19a9e3e1b18e4e722cac1ae54f7013212fa5
Emanuel Correia <guardeded@gmail.com>
2025-06-09 02:00:17 +0100

    Refactored some JS

--- js/app.js
+++ js/app.js
@@ -25,58 +25,53 @@ $(".nav-overlay").on("click", () => {
     }
 });
 
-// Enables the functionality of the "view more" button
+// collapsing animations for the projects section using jquery
 
-const moreDetailsButtons = [
-    "#netmatters-details",
-    "#project-2-details",
-    "#project-3-details",
-    "#project-4-details",
-    "#project-5-details",
-    "#project-6-details",
-];
+const detailsCards = {
+    "#netmatters-details": "#netmatters-card",
+    "#project-2-details": "#project-2-card",
+    "#project-3-details": "#project-3-card",
+    "#project-4-details": "#project-4-card",
+    "#project-5-details": "#project-5-card",
+    "#project-6-details": "#project-6-card",
+}
 
-const moreDetailsCards = [
-    "#netmatters-card",
-    "#project-2-card",
-    "#project-3-card",
-    "#project-4-card",
-    "#project-5-card",
-    "#project-6-card",
-];
+for(const button in detailsCards) {
+    $(detailsCards[button]).hide();
 
-// collapsing animations using jquery
-
-
-for (let i = 0; i < moreDetailsButtons.length; i++) {
-    $(moreDetailsCards[i]).hide();
-
-    $(moreDetailsButtons[i]).click(() => {
+    $(button).click(() => {
         $(".projects-work").slideUp(350);
-        $(moreDetailsCards[i]).slideDown(350);
+        $(detailsCards[button]).slideDown(350);
     });
 
-    $(".project-details .button").click(() => {
-        $(moreDetailsCards[i]).slideUp(350);
+    $(detailsCards[button] + " .button.close-slide").click(() => {
+        $(detailsCards[button]).slideUp(350);
         $(".projects-work").slideDown(350);
     });
 }
 
 // Remove the blur effect upon user scrolling scrollable elements
 
-function onScroll(event) {
-  const target = event.target;
-  target.classList.remove("scroll-me");
-  target.removeEventListener("scroll", onScroll);
+function removeScroll(event) {
+    const target = event.target;
+    target.classList.remove("scroll-me");
+    target.removeEventListener("scroll", removeScroll);
 }
 
-const scrollable = document.querySelectorAll(".scroll-me");
+
+function resetScroll(event) {
+    const illustration = event.target.parentElement.parentElement.querySelector(".details-illustration");
+    illustration.addEventListener("scroll", removeScroll)
+    illustration.classList.add("scroll-me")
+}
+
+const closeCardButtons = document.querySelectorAll(".button.close-slide")
+const scrollable = document.querySelectorAll(".details-illustration");
 
 for (let i = 0; i < scrollable.length; i++) {
-  scrollable[i].addEventListener("scroll", onScroll)
+    scrollable[i].addEventListener("scroll", removeScroll)
+    closeCardButtons[i].addEventListener("click", resetScroll)
 }
-
-
 
 // contact form validation
 
@@ -137,10 +132,9 @@ function validateForm() {
     return valid;
 }
 
-// loads particles js, particles config currently shoved in there, ideally to be offloaded on a seperate json file
-// particlesJS parameters: DOM element it targets, object config, callback function.
+/* load particles.js
+particlesJS.load parameters: DOM element it targets, object config, callback function.
 
-/*
 particlesJS.load('particles-js', 'particles.json', function() {
   console.log('particles.js loaded - callback');
 });
@@ -148,130 +142,127 @@ particlesJS.load('particles-js', 'particles.json', function() {
 
 particlesJS('particles-js',
   
-  {
-    "particles": {
-      "number": {
-        "value": 100,
-        "density": {
-          "enable": true,
-          "value_area": 800
+    {
+        "particles": {
+        "number": {
+            "value": 100,
+            "density": {
+            "enable": true,
+            "value_area": 800
+            }
+        },
+        "color": {
+            "value": "#ffffff"
+        },
+        "shape": {
+            "type": "circle",
+            "stroke": {
+            "width": 0,
+            "color": "#000000"
+            },
+            "polygon": {
+            "nb_sides": 5
+            },
+            "image": {
+            "src": "img/github.svg",
+            "width": 100,
+            "height": 100
+            }
+        },
+        "opacity": {
+            "value": 1,
+            "random": true,
+            "anim": {
+            "enable": true,
+            "speed": 0.6,
+            "opacity_min": 0.3,
+            "sync": false
+            }
+        },
+        "size": {
+            "value": 1.5,
+            "random": true,
+            "anim": {
+            "enable": false,
+            "speed": 1,
+            "size_min": 0.5,
+            "sync": false
+            }
+        },
+        "line_linked": {
+            "enable": false,
+            "distance": 150,
+            "color": "#ffffff",
+            "opacity": 0.4,
+            "width": 1
+        },
+        "move": {
+            "enable": true,
+            "speed": 0.1,
+            "direction": "top",
+            "random": true,
+            "straight": true,
+            "out_mode": "out",
+            "attract": {
+            "enable": false,
+            "rotateX": 600,
+            "rotateY": 1200
+            }
         }
-      },
-      "color": {
-        "value": "#ffffff"
-      },
-      "shape": {
-        "type": "circle",
-        "stroke": {
-          "width": 0,
-          "color": "#000000"
         },
-        "polygon": {
-          "nb_sides": 5
+        "interactivity": {
+        "detect_on": "canvas",
+        "events": {
+            "onhover": {
+            "enable": false,
+            "mode": "repulse"
+            },
+            "onclick": {
+            "enable": true,
+            "mode": "push"
+            },
+            "resize": true
         },
-        "image": {
-          "src": "img/github.svg",
-          "width": 100,
-          "height": 100
+        "modes": {
+            "grab": {
+            "distance": 400,
+            "line_linked": {
+                "opacity": 1
+            }
+            },
+            "bubble": {
+            "distance": 400,
+            "size": 40,
+            "duration": 2,
+            "opacity": 8,
+            "speed": 3
+            },
+            "repulse": {
+            "distance": 200
+            },
+            "push": {
+            "particles_nb": 1
+            },
+            "remove": {
+            "particles_nb": 2
+            }
         }
-      },
-      "opacity": {
-        "value": 1,
-        "random": true,
-        "anim": {
-          "enable": true,
-          "speed": 0.6,
-          "opacity_min": 0.3,
-          "sync": false
+        },
+        "retina_detect": true,
+        "config_demo": {
+        "hide_card": false,
+        "background_color": "#b61924",
+        "background_image": "",
+        "background_position": "50% 50%",
+        "background_repeat": "no-repeat",
+        "background_size": "cover"
         }
-      },
-      "size": {
-        "value": 1.5,
-        "random": true,
-        "anim": {
-          "enable": false,
-          "speed": 1,
-          "size_min": 0.5,
-          "sync": false
-        }
-      },
-      "line_linked": {
-        "enable": false,
-        "distance": 150,
-        "color": "#ffffff",
-        "opacity": 0.4,
-        "width": 1
-      },
-      "move": {
-        "enable": true,
-        "speed": 0.1,
-        "direction": "top",
-        "random": true,
-        "straight": true,
-        "out_mode": "out",
-        "attract": {
-          "enable": false,
-          "rotateX": 600,
-          "rotateY": 1200
-        }
-      }
-    },
-    "interactivity": {
-      "detect_on": "canvas",
-      "events": {
-        "onhover": {
-          "enable": false,
-          "mode": "repulse"
-        },
-        "onclick": {
-          "enable": true,
-          "mode": "push"
-        },
-        "resize": true
-      },
-      "modes": {
-        "grab": {
-          "distance": 400,
-          "line_linked": {
-            "opacity": 1
-          }
-        },
-        "bubble": {
-          "distance": 400,
-          "size": 40,
-          "duration": 2,
-          "opacity": 8,
-          "speed": 3
-        },
-        "repulse": {
-          "distance": 200
-        },
-        "push": {
-          "particles_nb": 1
-        },
-        "remove": {
-          "particles_nb": 2
-        }
-      }
-    },
-    "retina_detect": true,
-    "config_demo": {
-      "hide_card": false,
-      "background_color": "#b61924",
-      "background_image": "",
-      "background_position": "50% 50%",
-      "background_repeat": "no-repeat",
-      "background_size": "cover"
     }
-  }
 
 );
 
 // animejs to add animation to hero text
-
-const heroText = document.querySelectorAll(".floating-text");
-
-// function accepts value of a DOM object and animates it
+// function accepts value of a DOM element object and animates it
 
 function floatBlock(element) {
     function animateOnce() {
@@ -288,6 +279,8 @@ function floatBlock(element) {
     }
     animateOnce();
 }
+
+const heroText = document.querySelectorAll(".floating-text");
 
 for (i = 0; i < heroText.length; i++) {
     floatBlock(heroText[i]);
